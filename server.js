b1b3f6f23cbbc591cd09a3c4eb279c97da0221de
YasiruLaksara <yasirul.21@cse.mrt.ac.lk>
2024-07-11 17:04:09 +0530

    mongodb innitial implementation

--- server.js
+++ server.js
@@ -1,4 +1,50 @@
 const app = require("./app");
+const dotenv = require("dotenv"); //before that npm i dotenv
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+// console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  //.connect(process.env.DATABASE_LOCAL, {
+  .connect(DB)
+  .then(() => {
+    console.log("MongoDB Connected...");
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Tour name is required"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema); //collection
+
+const document = new Tour({ name: "Down South", rating: 4.9, price: 4500 });
+
+document
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = 3000;
 app.listen(port, () => {
